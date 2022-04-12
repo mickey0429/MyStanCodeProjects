ecc1021fa18e769ac269f20245499dca3f61c544
mickey0429 <103485141+mickey0429@users.noreply.github.com>
2022-04-12 15:31:19 +0800

    Add files via upload

---
+++ github/DoubleBeepersbyme.py
@@ -0,0 +1,36 @@
+"""
+File: DoubleBeepers.py
+Name:
+-------------------------------
+TODO:
+"""
+
+from karel.stanfordkarel import *
+
+
+def main():
+    """
+    Karel will double the beepers
+    """
+    move()
+    double beepers()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####### DO NOT EDIT CODE BELOW THIS LINE ########
+
+if __name__ == '__main__':
+    execute_karel_task(main)
